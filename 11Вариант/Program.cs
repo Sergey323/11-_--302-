c115c8af4a1f020d9d36f0511a813b7e23b77a44
Sergey Valeev <43838091+Sergey323@users.noreply.github.com>
2018-10-06 12:30:19 +0500

    11 Вариант
    Приложение позволяет определить, является ли заданный текст арифметическим выраже¬нием без скобок (т.е. в него входят только числа и/или знаки операций +,-,*,/).

---
+++ 11Вариант/Program.cs
@@ -0,0 +1,32 @@
+﻿using _11Вариант.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11Вариант
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите выражение для вычисления. Для выхода напишите exit");
+                string s = Console.ReadLine();
+                if (s == "exit")
+                    return;
+                var result = CalculateExpression.Calculate(s);
+                if (result == null)
+                    Console.WriteLine("Неправильный формат ввода");
+                else
+                    Console.WriteLine(result);
+            }
+        }
+    }
+}
+
+
+    
+
