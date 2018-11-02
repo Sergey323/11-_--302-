4819d216a69f99a37704af06bd47679e1683d004
Sergey Valeev <43838091+Sergey323@users.noreply.github.com>
2018-11-02 10:14:46 +0500

    Проверка арифметики

---
+++ проверка_арифметики/проверка_арифметики/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace проверка_арифметики
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+                {
+
+            
+                    Console.Write("ввведит строку ");
+                    string stroka = Console.ReadLine();
+                    Program p = new Program();
+                    bool resh = p.prover_na_orifmet(stroka);
+                    if (resh == true)
+                    {
+                        Console.Write("строка является арефметическим выражением");
+                    }
+                    else
+                    {
+                        Console.Write("строка не является арефметическим выражением");
+                    }
+            
+                    Console.ReadLine();
+            }
+        }
+        public bool prover_na_orifmet(string stroka)
+        {
+            if (stroka.Length < 2)
+            {
+                return false;
+            }
+            int schetchik=0;
+            for(int i = 0; i < stroka.Length; i++)
+            {
+                switch (stroka[i])
+                {
+                    case '1':
+                        break;
+                    case '2':
+                        break;
+                    case '3':
+                        break;
+                    case '4':
+                        break;
+                    case '5':
+                        break;
+                    case '6':
+                        break;
+                    case '7':
+                        break;
+                    case '8':
+                        break;
+                    case '9':
+                        break;
+                    case '0':
+                        break;
+                    case '+':
+                        break;
+                    case '-':
+                        break;
+                    case '*':
+                        break;
+                    case '/':
+                        break;
+                    case ' ':
+                        break;
+                    case '%':
+                        break;
+                    case '^':
+                        break;
+                    default:
+                        schetchik++;
+                        break;
+                }
+            }
+            if (schetchik > 0)
+            {
+                return false; 
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ проверка_арифметики/проверка_арифметики/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("проверка_арифметики")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("проверка_арифметики")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("a4009b8c-7430-4206-95de-aaab7a3ef84b")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
