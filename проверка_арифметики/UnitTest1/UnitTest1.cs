23d2b656a739a8c57e249b9b179e2711fbf9365b
Sergey Valeev <43838091+Sergey323@users.noreply.github.com>
2018-12-06 15:23:21 +0500

    Доработка программы
    Доработка программы  и добавление тестов

---
+++ проверка_арифметики/UnitTest1/UnitTest1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using проверка_арифметики;
+
+namespace UnitTest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // исходные данные
+            string stroka = "9*9=81";
+            bool expected = true;
+            Program g = new  Program();
+            bool rezult = g.prover_na_orifmet(stroka);
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            // исходные данные
+            string stroka = "9+9=18";
+            bool expected = true;
+            Program g = new  Program();
+            bool rezult = g.prover_na_orifmet(stroka);
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            // исходные данные
+            string stroka = "9-9=0";
+            bool expected = true;
+            Program g = new  Program();
+            bool rezult = g.prover_na_orifmet(stroka);
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            // исходные данные
+            string stroka = "9^9=1";
+            bool expected = false;
+            Program g = new  Program();
+            bool rezult = g.prover_na_orifmet(stroka);
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            // исходные данные
+            string stroka = "ctg(";
+            bool expected = false;
+            Program g = new  Program();
+            bool rezult = g.prover_na_orifmet(stroka);
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            // исходные данные
+            string stroka = "";
+            bool expected = false;
+            Program g = new Program();
+            bool rezult = g.prover_na_orifmet(stroka);
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            // исходные данные
+            string stroka = "ctg(tg(sin(1)/cos(2)))";
+            bool expected = true;
+            Program g = new Program();
+            bool rezult = g.prover_na_orifmet(stroka);
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+        }
+    }
+}
--- проверка_арифметики/проверка_арифметики/Program.cs
+++ проверка_арифметики/проверка_арифметики/Program.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace проверка_арифметики
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
@@ -30,6 +31,7 @@ namespace проверка_арифметики
                     Console.ReadLine();
             }
         }
+        
         public bool prover_na_orifmet(string stroka)
         {
             if (stroka.Length < 2)
@@ -37,7 +39,9 @@ namespace проверка_арифметики
                 return false;
             }
             int schetchik=0;
-            for(int i = 0; i < stroka.Length; i++)
+            string str;
+            Char[] znak=new Char[stroka.Length];
+            for (int i = 0; i < stroka.Length; i++)
             {
                 switch (stroka[i])
                 {
@@ -62,25 +66,161 @@ namespace проверка_арифметики
                     case '0':
                         break;
                     case '+':
+                        znak[0] = '+';
                         break;
                     case '-':
+                        znak[1] = '-';
                         break;
                     case '*':
+                        znak[2] = '*';
                         break;
                     case '/':
+                        znak[3] = '/';
                         break;
                     case ' ':
                         break;
                     case '%':
+                        znak[3] = '%';
                         break;
+
+
+
+
+
+
+
+                    case '=':
+                        string[] valid = stroka.Split('=');
+                        if((chislo(valid[1])==true)||(Dubl(valid[1]) == true))
+                        {
+                            string[] id = new string[3];
+                            foreach (Char da in znak)
+                            {
+                                if(da == 37)
+                                {
+                                    id = valid[0].Split('%');
+                                    if (Convert.ToDouble(id[0]) % Convert.ToDouble(id[1]) == Convert.ToDouble(valid[1]))
+                                    {
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        schetchik++;
+                                        break;
+                                    }
+                                }
+                                if (da == 42)
+                                {
+                                    id = valid[0].Split('*');
+                                    if (Convert.ToDouble(id[0]) * Convert.ToDouble(id[1]) == Convert.ToDouble(valid[1]))
+                                    {
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        schetchik++;
+                                        break;
+                                    }
+                                }
+                                if (da == 43)
+                                {
+                                    id = valid[0].Split('+');
+                                    if (Convert.ToDouble(id[0]) + Convert.ToDouble(id[1]) == Convert.ToDouble(valid[1]))
+                                    {
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        schetchik++;
+                                        break;
+                                    }
+                                }
+                                if (da == 47)
+                                {
+                                    id = valid[0].Split('/');
+                                    if (Convert.ToDouble(id[0]) / Convert.ToDouble(id[1]) == Convert.ToDouble(valid[1]))
+                                    {
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        schetchik++;
+                                        break;
+                                    }
+                                }
+                                if (da == 45)
+                                {
+                                    id = valid[0].Split('-');
+                                    if (Convert.ToDouble(id[0]) - Convert.ToDouble(id[1]) == Convert.ToDouble(valid[1]))
+                                    {
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        schetchik++;
+                                        break;
+                                    }
+                                }
+                                if (da == 94)
+                                {
+                                    id = valid[0].Split('^');
+                                    if (Math.Pow(Convert.ToDouble(id[0]),Convert.ToDouble(id[1])) == Convert.ToDouble(valid[1]))
+                                    {
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        schetchik++;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+
+                        break;
+                        
+
                     case '^':
+                        znak[4] = '^';
+                        break;
+                    case 'c':
+                        str = Convert.ToString(stroka[i] + stroka[i + 1] + stroka[i + 2] + stroka[i + 3]);
+                        if (str == "358" || str=="365")
+                        {
+                            i = i + 3;
+                            schetchik++;
+                            
+
+                        }
+
+                        else
+                            schetchik++;
+                        break;
+                     case 's':
+                        str = Convert.ToString(stroka[i] + stroka[i + 1] + stroka[i + 2] + stroka[i + 3]);
+                        if (str == "370")
+                        {
+                            i = i + 3;
+                            schetchik++;
+                        }
+                        break;
+                    case 't':
+                        str = Convert.ToString(stroka[i] + stroka[i + 1] + stroka[i + 2]);
+                        if (str == "259")
+                        {
+                            i = i + 2;
+                            schetchik++;
+                        }
+                        break;
+                    case ')':
+                        schetchik--;
                         break;
                     default:
                         schetchik++;
                         break;
                 }
             }
-            if (schetchik > 0)
+            if (schetchik != 0)
             {
                 return false; 
             }
@@ -89,5 +229,27 @@ namespace проверка_арифметики
                 return true;
             }
         }
+
+        public bool chislo(string test)
+        {
+            int num;
+            bool isNum = int.TryParse(test, out num);
+            if (isNum)
+                return true;
+            else
+                return false;
+        }
+
+
+        public bool Dubl(string test)
+        {
+            double num;
+            bool isNum = double.TryParse(test, out num);
+            if (isNum)
+                return true;
+            else
+                return false;
+        }
+
     }
 }
